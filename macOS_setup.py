f55c3cdb19983eddf3ae1cddb45af8d8bf2729f7
Catalyst-42 <nikis16042004@gmail.com>
2024-08-28 12:25:56 +0300

    Windows icons

--- macOS_setup.py
+++ macOS_setup.py
@@ -18,7 +18,7 @@ OPTIONS = {
         'CFBundleDisplayName': APP_NAME,
         'CFBundleGetInfoString': 'Mouse can walk',
         'CFBundleIdentifier': 'com.catalyst.mousewalk',
-        'CFBundleVersion': '1.0.1',
+        'CFBundleVersion': '1.0.0',
         'CFBundleShortVersionString': '1.0.0',
         'NSHumanReadableCopyright': u'Copyright © 2024, Catalyst',
         'LSUIElement': True,
